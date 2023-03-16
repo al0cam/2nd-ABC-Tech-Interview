8f597346c11f74a1f1bdf3e7862be8cbda35bc76
Al0cam <benjaminsikac@gmail.com>
2023-03-16 15:33:06 +0100

    Modified the controller to use the service and added CRUD methods

--- src/main/java/com/interview/app/WebApp/Controllers/RacunController.java
+++ src/main/java/com/interview/app/WebApp/Controllers/RacunController.java
@@ -3,17 +3,23 @@ package com.interview.app.WebApp.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.interview.app.WebApp.Models.Racun;
-import com.interview.app.WebApp.Repositories.RacunRepository;
+import com.interview.app.WebApp.Service.RacunService;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class RacunController {
 
 	@Autowired
-	RacunRepository racunRepository;
+	RacunService racunService;
 
     @GetMapping("/greeting")
 	public String greeting() {
@@ -22,6 +28,27 @@ public class RacunController {
 	
 	@GetMapping("/")
 	public List<Racun> getAllRacun() {
-		return racunRepository.findAll();
+		return racunService.findAll();
 	}
+
+	@GetMapping("/racun/{id}")
+	public Racun getRacunById(@PathVariable("id") Long id) {
+		return racunService.findRacunById(id);
+	}
+
+	@PostMapping("/racun/add")
+	public Racun addRacun(@RequestBody Racun racun) {
+		return racunService.addRacun(racun);
+	}
+
+	@PostMapping("/racun/update")
+	public Racun updateRacun(@RequestBody Racun racun) {
+		return racunService.updateRacun(racun);
+	}
+
+	@DeleteMapping("/racun/delete/{id}")
+	public void deleteRacun(@PathVariable("id") Long id) {
+		racunService.deleteRacun(id);
+	}
+
 }
