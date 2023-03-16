8f7bee249580abb95e68ea040e7e7714220ae2eb
Al0cam <benjaminsikac@gmail.com>
2023-03-16 14:37:44 +0100

    Created a controller to test the Model part of the app

--- src/main/java/com/interview/app/WebApp/Controllers/RacunController.java
+++ src/main/java/com/interview/app/WebApp/Controllers/RacunController.java
@@ -1,12 +1,27 @@
 package com.interview.app.WebApp.Controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.interview.app.WebApp.Models.Racun;
+import com.interview.app.WebApp.Repositories.RacunRepository;
+
 @RestController
 public class RacunController {
+
+	@Autowired
+	RacunRepository racunRepository;
+
     @GetMapping("/greeting")
 	public String greeting() {
 		return "Hello World!";
+	}
+	
+	@GetMapping("/")
+	public List<Racun> getAllRacun() {
+		return racunRepository.findAll();
 	}
 }
