1e80a64029dfef9bc02081ab6d8beefc4025afd8
Al0cam <benjaminsikac@gmail.com>
2023-03-16 14:37:14 +0100

    Changed schema to have a primary key so duplicate entries aren't allowed

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 create table if not exists racun (
-    racun_id integer,
+    racun_id integer primary key,
     iban varchar(20),
     tip_racuna integer,
     valuta varchar(3),
@@ -8,14 +8,11 @@ create table if not exists racun (
     datum_zatvaranja date
 );
 
-
-
 -- For some reason the following inserts don't work in the dockerized psql; on the regular psql they work fine
 -- insert into racun values(1,'HR3455345442',1,'USD',1,'21.12.2012','16.3.2020');
 -- insert into racun values(2,'DE3232366551',1,'EUR',2,'15.6.2001',NULL);
 -- insert into racun values(3,'HR2134666632',2,'EUR',1,'17.3.2020',NULL);
 
-
-insert into racun values(1,'HR3455345442',1,'HRK',1,to_date('21.12.2012','dd.mm.yyyy'),to_date('16.3.2020','dd.mm.yyyy'));
-insert into racun values(2,'DE3232366551',1,'EUR',2,to_date('15.6.2001','dd.mm.yyyy'),NULL);
-insert into racun values(3,'HR2134666632',2,'EUR',1,to_date('17.3.2020','dd.mm.yyyy'),NULL);
+insert into racun values(1,'HR3455345442',1,'HRK',1,to_date('21.12.2012','dd.mm.yyyy'),to_date('16.3.2020','dd.mm.yyyy')) ON CONFLICT DO NOTHING;
+insert into racun values(2,'DE3232366551',1,'EUR',2,to_date('15.6.2001','dd.mm.yyyy'),NULL) ON CONFLICT DO NOTHING;
+insert into racun values(3,'HR2134666632',2,'EUR',1,to_date('17.3.2020','dd.mm.yyyy'),NULL) ON CONFLICT DO NOTHING;
